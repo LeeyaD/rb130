01b716d09b85927dfa7e6bffd6d4750d391a18ca
Leeya Davis <leeya463@gmail.com>
2021-12-30 20:08:23 -0500

    finished remaining exercises in easy testing

---
+++ exercises/easy_testing/06_exception_assertions.rb
@@ -0,0 +1,2 @@
+# Write a minitest assertion that will fail unless employee.hire raises a NoExperienceError exception.
+assert_raises(NoExperienceError) { employee.hire }
---
+++ exercises/easy_testing/09_same_object_assertions.rb
@@ -0,0 +1,5 @@
+# Write a test that will fail if list and the return value of list.process are different objects.
+
+assert_same(list, list.process)
+
+# #assert_same uses #equal?
