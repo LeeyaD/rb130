eaabb28d3ac663f53e041760e75d1262b51514ce
Leeya Davis <leeya463@gmail.com>
2022-01-08 08:11:34 -0500

    Completed Easy 1, #6 exercise

--- exercises/easy_1/06_iterators_true_for_any.rb
+++ exercises/easy_1/06_iterators_true_for_any.rb
@@ -9,12 +9,52 @@
 # If the Array is empty, any? should return false, regardless of how the block is defined.
   
 # Your method may not use any standard ruby method that is named all?, any?, none?, or one?.
-  
 
-any?([1, 3, 5, 6]) { |value| value.even? } == true
-any?([1, 3, 5, 7]) { |value| value.even? } == false
-any?([2, 4, 6, 8]) { |value| value.odd? } == false
-any?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
-any?([1, 3, 5, 7]) { |value| true } == true
-any?([1, 3, 5, 7]) { |value| false } == false
-any?([]) { |value| true } == false
+=begin
+PROBLEM
+Create the #any? method that behaves similarly to Array#any? The passed in array will be iterated over and each element passed to the block. The return value of the block on each iteration will determine if our #any? returns true or false. If the block returns a value of true for any element, then #any? returns true. Otherwise, #any? returns false.
+
+input: 1 argument, 'array' & 1 block
+- block will be the criteria for when #any? returns true
+
+output: a boolean object, true or false
+- true if the block returns true
+- false if the block never returns true
+
+Notes:
+- #any? should stop iterating over the collection the first time the block returns true
+- if the Array is empty, #any? should return false, regardless of how the block is defined
+- we cannot use all?, any?, none?, or one?
+
+Questions:
+- How to handle an Array that contains non-integer values?
+-- raise error? Hmm..I thnk it'll be handled by block code
+
+DS: 
+-array
+
+ALGO:
+- create a method that takes 1 arg & a block
+- iterate through given array, passing each element to the block
+- capture the return value of the block
+-- if the return value is true, stop iterating & return true from the method
+-- if the return value is false, continue iterating through the method
+- if iteration finishes and we're still in the method (i.e. the block never returned true), return false
+=end
+
+def any?(array)
+  array.each do |el|
+    block_return = yield(el)
+    return true if block_return == true
+  end
+
+  return false
+end
+
+p any?([1, 3, 5, 6]) { |value| value.even? } == true
+p any?([1, 3, 5, 7]) { |value| value.even? } == false
+p any?([2, 4, 6, 8]) { |value| value.odd? } == false
+p any?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
+p any?([1, 3, 5, 7]) { |value| true } == true
+p any?([1, 3, 5, 7]) { |value| false } == false
+p any?([]) { |value| true } == false
