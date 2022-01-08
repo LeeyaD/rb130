91436d2964604e9310ad75c7f0f7ff38139a4d28
Leeya Davis <leeya463@gmail.com>
2022-01-07 22:55:59 -0500

    Experimented with tests in file

--- car_test.rb
+++ car_test.rb
@@ -17,20 +17,20 @@ class CarTest < Minitest::Test
     car2.name = "Kim"
 
     assert_equal(car1, car2)
-    assert_same(car1, car2)
+    # assert_same(car1, car2)
   end
 
-  # def test_wheels
-  #   assert_equal(4, @car.wheels)
-  # end
+  def test_wheels
+    assert_equal(4, @car.wheels)
+  end
 
-  # def test_car_exists
-  #   assert(@car)
-  # end
+  def test_car_exists
+    assert(@car)
+  end
 
-  # def test_name_is_nil
-  #   assert_nil(@car.name)
-  # end
+  def test_name_is_nil
+    assert_nil(@car.name)
+  end
 
   def test_raise_initialize_with_arg
     assert_raises(ArgumentError) do
@@ -38,14 +38,15 @@ class CarTest < Minitest::Test
     end
   end
 
-  # def test_instance_of_car
-  #   assert_instance_of(Car, @car)
-  # end
+  def test_instance_of_car
+    assert_instance_of(Car, @car)
+    refute_instance_of(Array, @car)
+  end
 
-  # def test_includes_car
-  #   arr = [1, 2, 3]
-  #   arr << @car
+  def test_includes_car
+    arr = [1, 2, 3]
+    arr << @car
 
-  #   assert_includes(arr, @car)
-  # end
+    assert_includes(arr, @car)
+  end
 end
